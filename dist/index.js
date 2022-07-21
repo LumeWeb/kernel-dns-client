ac9cc4cf0dc58a5047a7b0c3bcc70705f51ae65f
Derrick Hammer <derrick@derrickhammer.com>
2022-07-21 13:22:02 -0400

    *Update dist

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,22 @@
-import { callModule as callModuleKernel, connectModule as connectModuleKernel, } from "libkernel";
-import { callModule as callModuleModule, connectModule as connectModuleModule, } from "libkmodule";
 const DNS_MODULE = "AQBLKpieqOfKVRgMa8k45P4S_ILYgJmswVso4vT1qzoG-A";
 let callModule, connectModule;
-if (window.document) {
-    callModule = callModuleKernel;
-    connectModule = connectModuleKernel;
-}
-else {
-    callModule = callModuleModule;
-    connectModule = connectModuleModule;
+async function loadLibs() {
+    if (callModule && connectModule) {
+        return;
+    }
+    if (typeof window !== "undefined" && window?.document) {
+        const pkg = (await import("libkernel"));
+        callModule = pkg.callModule;
+        connectModule = pkg.connectModule;
+    }
+    else {
+        const pkg = (await import("libkmodule"));
+        callModule = pkg.callModule;
+        connectModule = pkg.connectModule;
+    }
 }
 export async function resolve(domain, params, force = false) {
+    await loadLibs();
     const [resp, err] = await callModule(DNS_MODULE, "resolve", { domain, params, force });
     if (err) {
         throw new Error(err);
