e3697e8f5de82179081daf5ec39804b9b747a259
Derrick Hammer <derrick@derrickhammer.com>
2023-02-19 16:27:37 -0500

    *Update dist

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,10 @@
 import { Client, factory } from "@lumeweb/libkernel-universal";
 const MODULE = "PACYNuYbp_5hgCjMK16EGcytB9QCxDLe4_uitahwePdeaA";
 export class DnsClient extends Client {
-    async register() {
-        return this.callModuleReturn("register");
+    async register(module) {
+        const bag = await this.loadBound(module);
+        const ret = await bag.callModule("register");
+        this.handleError(ret);
     }
     async clear() {
         return this.callModuleReturn("clear");
