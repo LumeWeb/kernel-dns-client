096da60801f168265dcd9287c44d01ccebd3aba3
Derrick Hammer <derrick@derrickhammer.com>
2023-02-19 16:27:24 -0500

    *Update register to use loadBound

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import { Client, factory } from "@lumeweb/libkernel-universal";
 const MODULE = "PACYNuYbp_5hgCjMK16EGcytB9QCxDLe4_uitahwePdeaA";
 
 export class DnsClient extends Client {
-  public async register(): Promise<void> {
-    return this.callModuleReturn("register");
+  public async register(module: string): Promise<void> {
+    const bag = await this.loadBound(module);
+    const ret = await bag.callModule("register");
+    this.handleError(ret);
   }
   public async clear(): Promise<void> {
     return this.callModuleReturn("clear");
