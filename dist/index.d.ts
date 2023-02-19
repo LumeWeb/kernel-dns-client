91b1c89bc40aa72b0c54f1d3104e9b40b678aff5
Derrick Hammer <derrick@derrickhammer.com>
2023-02-19 16:22:59 -0500

    *Update dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,9 @@
-import { ResolverOptions } from "@lumeweb/resolver-common";
-export declare function resolve(
-  domain: string,
-  options?: ResolverOptions,
-  bypassCache?: boolean
-): Promise<any>;
-export declare function register(): Promise<void>;
-export declare function clear(): Promise<void>;
-export declare function getResolvers(): Promise<any>;
-export declare function ready(): Promise<any>;
+import { Client } from "@lumeweb/libkernel-universal";
+export declare class DnsClient extends Client {
+  register(): Promise<void>;
+  clear(): Promise<void>;
+  getResolvers(): Promise<void>;
+  ready(): Promise<void>;
+}
+export declare const createClient: (...args: any) => DnsClient;
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -1,54 +1,17 @@
-import { DNS_RECORD_TYPE } from "@lumeweb/resolver-common";
-const DNS_MODULE = "AQBLKpieqOfKVRgMa8k45P4S_ILYgJmswVso4vT1qzoG-A";
-let callModule, connectModule;
-async function loadLibs() {
-    if (callModule && connectModule) {
-        return;
+import { Client, factory } from "@lumeweb/libkernel-universal";
+const MODULE = "PACYNuYbp_5hgCjMK16EGcytB9QCxDLe4_uitahwePdeaA";
+export class DnsClient extends Client {
+    async register() {
+        return this.callModuleReturn("register");
     }
-    if (typeof window !== "undefined" && window?.document) {
-        const pkg = await import("libkernel");
-        callModule = pkg.callModule;
-        connectModule = pkg.connectModule;
+    async clear() {
+        return this.callModuleReturn("clear");
     }
-    else {
-        const pkg = await import("libkmodule");
-        callModule = pkg.callModule;
-        connectModule = pkg.connectModule;
+    async getResolvers() {
+        return this.callModuleReturn("clear");
+    }
+    async ready() {
+        return this.callModuleReturn("ready");
     }
 }
-export async function resolve(domain, options = { type: DNS_RECORD_TYPE.CONTENT }, bypassCache = false) {
-    await loadLibs();
-    const [resp, err] = await callModule(DNS_MODULE, "resolve", {
-        domain,
-        options,
-        bypassCache,
-    });
-    if (err) {
-        throw new Error(err);
-    }
-    return resp;
-}
-export async function register() {
-    await loadLibs();
-    await callModule(DNS_MODULE, "register");
-}
-export async function clear() {
-    await loadLibs();
-    await callModule(DNS_MODULE, "clear");
-}
-export async function getResolvers() {
-    await loadLibs();
-    const [resp, err] = await callModule(DNS_MODULE, "getResolvers");
-    if (err) {
-        throw new Error(err);
-    }
-    return resp;
-}
-export async function ready() {
-    await loadLibs();
-    const [resp, err] = await callModule(DNS_MODULE, "ready");
-    if (err) {
-        throw new Error(err);
-    }
-    return resp;
-}
+export const createClient = factory(DnsClient, MODULE);
