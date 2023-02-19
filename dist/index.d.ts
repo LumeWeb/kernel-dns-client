a0800a0feb1bb4488b3daba5f6206ff7c8a7f8ad
Derrick Hammer <derrick@derrickhammer.com>
2023-02-19 16:35:34 -0500

    *Update dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,7 +2,7 @@ import { Client } from "@lumeweb/libkernel-universal";
 export declare class DnsClient extends Client {
   register(): Promise<void>;
   clear(): Promise<void>;
-  getResolvers(): Promise<string>;
+  getResolvers(): Promise<string[]>;
   ready(): Promise<void>;
 }
 export declare const createClient: (...args: any) => DnsClient;
