1e8924d5ac8552e0fb71f62b3eee94651a146c05
Derrick Hammer <derrick@derrickhammer.com>
2023-04-09 14:15:40 -0400

    *add registerResolver method

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,6 @@ import { createModule, ResolverModule } from "./module.js";
 const MODULE = "vAA-phmM1ztqu8zGBKF2nM3b8N_IQGWlB6J_doi2FhqS-A";
 
 export class DnsClient extends Client {
-  public async register(): Promise<void> {
-    return this.callModuleReturn("register");
-  }
-  public async clear(): Promise<void> {
-    return this.callModuleReturn("clear");
-  }
   get resolvers(): Promise<Set<ResolverModule>> {
     return this.callModuleReturn("getResolvers").then((resolvers: string[]) => {
       return new Set(
@@ -24,6 +18,21 @@ export class DnsClient extends Client {
       );
     });
   }
+
+  public async register(): Promise<void> {
+    return this.callModuleReturn("register");
+  }
+
+  public async registerResolver(module: string): Promise<void> {
+    const bag = await this.loadBound(module);
+    const ret = await bag.callModule("register");
+    return ret[0];
+  }
+
+  public async clear(): Promise<void> {
+    return this.callModuleReturn("clear");
+  }
+
   public async resolve(
     domain: string,
     options: ResolverOptions = { type: DNS_RECORD_TYPE.CONTENT },
